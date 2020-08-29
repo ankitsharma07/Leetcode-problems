c63008ce33837fe9cfd47bd1ff155b4cd1ee56e9
Ankit Sharma <ankitsharma0711@icloud.com>
2020-08-29 18:44:31 +0530

    problems

---
+++ 226_InvertBinaryTree.cpp
@@ -0,0 +1,47 @@
+/*
+* Invert Binary Tree:
+* Approach 1 -- Use recursion
+*
+* */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+    struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL){
+            return NULL;
+        }
+
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+
+        root->left = right;
+        root->right = left;
+
+        return root;
+    }
+};
