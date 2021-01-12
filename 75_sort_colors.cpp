e30259041ad0dd90dc90e4ad32c92da315cb3995
Ankit Sharma <nez.dev@icloud.com>
2021-01-12 11:35:54 +0530

    just another commit

---
+++ 75_sort_colors.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+        void sortColors(vector<int>& nums) {
+            int low = 0;
+            int mid = 0;
+            int high = nums.size() - 1;
+
+
+            // iterate till the elements are sorted
+            while(mid <= high) {
+                switch (nums[mid]) {
+                    case 0:
+                        swap(nums[low++], nums[mid++]);
+                        break;
+
+                    case 1:
+                        mid++;
+                        break;
+
+                    case 2:
+                        swap(nums[mid], nums[high--]);
+                        break;
+                }
+            }
+    }
+};
