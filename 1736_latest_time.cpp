bfbf3648d0686adad33843b57d1eb47edf20503e
Ankit Sharma <nez.dev@icloud.com>
2021-01-24 23:27:06 +0530

    string based

---
+++ 1736_latest_time.cpp
@@ -0,0 +1,29 @@
+// ternary operator: (condition) ? expression1 : expression2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string maximumTime(string time) {
+        if (time[0]=='?') {
+            time[0] = (time[1] <= '3' || time[1] == '?') ? '2' : '1';
+        }
+
+        if (time[1] == '?'){
+            time[1] = time[0]=='2' ? '3' : '9';
+        }
+
+        if (time[3] == '?') {
+            time[3] = '5';
+        }
+
+        if (time[4] == '?'){
+            time[4] = '9';
+        }
+
+        return time;
+    }
+};
