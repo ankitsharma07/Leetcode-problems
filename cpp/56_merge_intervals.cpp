1caab036e7b559b854119238c6744af41d28c13f
Ankit Sharma <nez.dev@icloud.com>
2021-01-31 00:07:55 +0530

    last array basic

--- cpp/56_merge_intervals.cpp
+++ cpp/56_merge_intervals.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> merged_intervals;
+
+        if(intervals.size() == 0) {
+            return merged_intervals;
+        }
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<int> temp_interval = intervals[0];
+
+        for (auto i : intervals) {
+            if(i[0] <= temp_interval[1]) {
+                temp_interval[1] = max(i[1], temp_interval[1]);
+            }
+            else {
+                merged_intervals.push_back(temp_interval);
+                temp_interval = i;
+            }
+        }
+
+        merged_intervals.push_back(temp_interval);
+        return merged_intervals;
+    }
+};
