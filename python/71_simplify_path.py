d65094668804e82a48e64c46a55a48417f3e51b6
Ankit Sharma <nez.dev@icloud.com>
2021-02-07 23:10:46 +0530

    updates

---
+++ python/71_simplify_path.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = [ ]
+
+        for token in path.split('/'):
+            if token in ('', '.'):
+                pass
+            elif token == '..':
+                if stack: stack.pop()
+            else:
+                stack.append(token)
+
+        return '/' + '/'.join(stack)
