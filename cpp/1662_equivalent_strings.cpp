45293f74aad891388ce4f3fccd901493fdb9e11f
Ankit Sharma <nez.dev@icloud.com>
2021-03-29 22:15:45 +0530

    lul

---
+++ cpp/1662_equivalent_strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = accumulate(word1.begin(), word1.end(), string{});
+        string s2 = accumulate(word2.begin(), word2.end(), string{});
+
+        return s1 == s2;
+    }
+};
