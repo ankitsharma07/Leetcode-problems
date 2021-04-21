f224017401e776076e3ca0c4603366edb2799502
Ankit Sharma <nez.dev@icloud.com>
2021-04-21 19:36:12 +0530

    wtf

---
+++ cpp/506_fib_num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+};
